1dee0b13fdd4fdfffa910278c0ee3bc1a8550d36
Bhuvaneswari <mailtobhuvana04@gmail.com>
2024-05-29 09:36:34 -0700

    first commit

---
+++ src/main/java/com/example/aprilbatch_liveproject/AprilbatchLiveprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.aprilbatch_liveproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilbatchLiveprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprilbatchLiveprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aprilbatch_liveproject/AprilbatchLiveprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aprilbatch_liveproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprilbatchLiveprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
